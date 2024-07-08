dd871528517b99fb915343f4dd83200eeced96a3
najas <muhammednajaas24@gmail.com>
2024-07-08 14:32:30 +0530

    Aos updated

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,9 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173
   },
-  base: '/Car-Sell-React-Tailwind/',  // Ensure this matches your repo name
+  build: {
+    rollupOptions: {
+      external: ['aos']
+    }
+  }
 });
