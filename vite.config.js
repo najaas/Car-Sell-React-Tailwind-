1dbe0edf39ed2fa01b64c47e7becaa366e92dd94
najas <muhammednajaas24@gmail.com>
2024-07-08 12:27:25 +0530

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 5173
   },
-  base: '/Car-Sell-React-Tailwind-/',  // Add this line
+  base: '/Car-Sell-React-Tailwind/',  // Ensure this matches your repo name
 });
