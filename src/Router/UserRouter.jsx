9675f2e94c1f03a8fa1931dca9fdc190836e79f7
najas <muhammednajaas24@gmail.com>
2024-10-17 01:01:39 +0530

    change9

--- src/Router/UserRouter.jsx
+++ src/Router/UserRouter.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "@/Pages/user/Login";
-import Signup from "../Pages/user/Signup";
-import Home from "../Pages/user/Home";
-import Forget from "../Pages/user/ForgetPassword/Forgetpass";
-import Otp from "../Pages/user/ForgetPassword/Otppage";
-import NewPassword from "../Pages/user/ForgetPassword/Newpassword";
-import Success from "../Pages/user/ForgetPassword/Success";
-import Card from "../Components/User/Card";
-import Card1 from "../Components/User/Card1";
-import Header from "../Components/User/Header/Head";
-import Card2 from "../Pages/user/Card2";
-import UserLayout from "../Layout/Layout";
+import Signup from "@/Pages/user/Signup";
+import Home from "@/Pages/user/Home";
+import Forget from "@/Pages/user/ForgetPassword/Forgetpass";
+import Otp from "@/Pages/user/ForgetPassword/Otppage";
+import NewPassword from "@/Pages/user/ForgetPassword/Newpassword";
+import Success from "@/Pages/user/ForgetPassword/Success";
+import Card from "@/Components/User/Card";
+import Card1 from "@/Components/User/Card1";
+import Header from "@/Components/User/Header/Head";
+import Card2 from "@/Pages/user/Card2";
+import UserLayout from "@/Layout/Layout";
 
 
 const UserRouter = () => {
