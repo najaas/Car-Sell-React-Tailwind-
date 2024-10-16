1b359dec4b04ed6d4af2f7bbf558d5a8f9c0a70b
najas <muhammednajaas24@gmail.com>
2024-10-17 01:46:32 +0530

    aos installation

--- src/tempPages/Admin/Auctionlist.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Auctionlist = () => {
-  return (
-    <div>Auctionlist</div>
-  )
-}
-
-export default Auctionlist
--- src/tempPages/Admin/Userlist.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Userlist = () => {
-  return (
-    <div>Userlist</div>
-  )
-}
-
-export default Userlist
